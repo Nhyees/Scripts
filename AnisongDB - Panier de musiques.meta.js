0d7c4f1744d87815473c388ffb6b8c0090a8fb31
HUYNH HUU NHI <48217568+Nhyees@users.noreply.github.com>
2026-05-08 17:47:56 +0200

    Update updateURL for AnisongDB script

--- AnisongDB - Panier de musiques.meta.js
+++ AnisongDB - Panier de musiques.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         AnisongDB - Panier de musiques
 // @version      1.0
-// @updateURL    https://raw.githubusercontent.com/.../AnisongDB%20-%20Panier%20de%20musiques.meta.js
+// @updateURL    https://raw.githubusercontent.com/Nhyees/Scripts/refs/heads/main/AnisongDB%20-%20Panier%20de%20musiques.meta.js
 // @downloadURL  https://raw.githubusercontent.com/.../AnisongDB%20-%20Panier%20de%20musiques.user.js
 // ==/UserScript==

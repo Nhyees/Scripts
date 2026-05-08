ec4a2fc81ebeeee59707223cde52bd5f04f15c7d
HUYNH HUU NHI <48217568+Nhyees@users.noreply.github.com>
2026-05-08 17:36:18 +0200

    Create AnisongDB - Panier de musiques.meta.js

---
+++ AnisongDB - Panier de musiques.meta.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         AnisongDB - Panier de musiques
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Ajouter des musiques dans un panier et exporter en JSON depuis AnisongDB
+// @author       Nhyees
+// @match        https://anisongdb.com/*
+// @updateURL    https://raw.githubusercontent.com/Nhyees/Scripts/main/anisongdb-panier.meta.js
+// @downloadURL  https://raw.githubusercontent.com/Nhyees/Scripts/main/anisongdb-panier.user.js
+// @grant        none
+// ==/UserScript==
